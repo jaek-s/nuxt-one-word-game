7685b85b5a4f4e1607cbc5adbdcca2c8e20009dd
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-04-29 13:00:36 -0600

    adjust nuxt config format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        "@nuxtjs/tailwindcss",
-        "@nuxtjs/eslint-module",
-        [
-            "nuxt-vuefire",
-            {
-                config: {
-                    apiKey: "AIzaSyCFR8hkuNn09BV3hq6CUUHA7DTKJw8iS58",
-                    authDomain: "js-one-word-game.firebaseapp.com",
-                    projectId: "js-one-word-game",
-                    storageBucket: "js-one-word-game.appspot.com",
-                    messagingSenderId: "262705775305",
-                    appId: "1:262705775305:web:ad67a41f7c4539eac4c318",
-                },
-            },
-        ],
-    ],
+    modules: ["@nuxtjs/eslint-module", "@nuxtjs/tailwindcss", "nuxt-vuefire"],
+    vuefire: {
+        config: {
+            apiKey: "AIzaSyCFR8hkuNn09BV3hq6CUUHA7DTKJw8iS58",
+            authDomain: "js-one-word-game.firebaseapp.com",
+            projectId: "js-one-word-game",
+            storageBucket: "js-one-word-game.appspot.com",
+            messagingSenderId: "262705775305",
+            appId: "1:262705775305:web:ad67a41f7c4539eac4c318",
+        },
+    },
 });
