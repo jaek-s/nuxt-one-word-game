06f85fd63a91af3430b895e1f3074bda974e7963
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-04-30 01:05:45 -0600

    tweak html structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
-    <main
-        class="max-w-screen-lg mx-auto mt-16 flex flex-col justify-center items-center gap-8 sm:flex-row"
+    <div
+        class="max-w-screen-sm mx-auto mt-16 px-8 flex flex-col justify-between gap-8 items-center sm:items-stretch sm:flex-row"
     >
         <header class="w-64">
-            <h1 class="text-7xl font-black">One Word Game</h1>
+            <h1 class="text-7xl font-black">One<br />Word<br />Game</h1>
             <h2 class="text-lg font-medium text-stone-400">
-                Write a story with friends, one word at a time.
+                Write a story with friends,<br />one word at a time.
             </h2>
         </header>
-        <slot />
-    </main>
+        <main class="grow flex">
+            <slot />
+        </main>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts"></script>
-
 <template>
-    <section class="flex flex-col gap-4 w-64 justify-center">
-        <NuxtLink to="create-account" class="btn-primary h-16 text-lg font-medium">
+    <div class="flex flex-col gap-4 w-full justify-center items-center">
+        <NuxtLink to="create-account" class="btn-primary w-64 h-16 text-lg font-medium">
             Create an Account
         </NuxtLink>
-        <button class="btn">Log In</button>
-    </section>
+        <button class="btn w-64">Log In</button>
+    </div>
 </template>
