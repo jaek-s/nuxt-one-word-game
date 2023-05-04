0ad94828d5201be8bbb6d90c0d51c79ac8e1b421
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-05-03 22:40:51 -0600

    tiny amount of cleanup

--- layouts/unauthenticated.vue
+++ layouts/unauthenticated.vue
@@ -2,17 +2,17 @@
     <div
         class="max-w-screen-sm mx-auto mt-16 px-8 flex flex-col justify-between gap-8 items-center sm:items-stretch sm:flex-row"
     >
-        <NuxtLink to="/">
-            <header class="w-64">
+        <header class="w-64">
+            <NuxtLink to="/">
                 <h1 class="text-7xl font-black leading-none">
                     One<br />Word<br />Game
                 </h1>
                 <h2 class="text-lg font-medium text-stone-400">
                     Write a story with friends,<br />one word at a time.
                 </h2>
-            </header>
-        </NuxtLink>
-        <main class="grow flex">
+            </NuxtLink>
+        </header>
+        <main class="grow">
             <slot />
         </main>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@ watchEffect(() => {
 </script>
 
 <template>
-    <div class="flex flex-col gap-4 w-full justify-center items-center">
+    <div class="h-full flex flex-col gap-4 w-full justify-center items-center">
         <NuxtLink
             to="/create-account"
             class="btn-primary w-64 h-16 text-lg font-medium"
