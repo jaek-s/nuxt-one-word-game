ede5630f66865fb974c0bbf73eb3179926dba44b
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-05-04 21:59:33 -0600

    add a button that puts a story in the DB

--- pages/stories.vue
+++ pages/stories.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-import { collection } from "firebase/firestore";
+import { nanoid } from "nanoid";
+import { collection, addDoc } from "firebase/firestore";
+
 definePageMeta({
     middleware: ["check-authentication"],
 });
 const db = useFirestore();
-const stories = useCollection(collection(db, "stories"));
+const firestoreStories = collection(db, "stories");
+const stories = useCollection(firestoreStories);
+
+const addItem = () => {
+    addDoc(firestoreStories, {
+        hello: nanoid(),
+    });
+};
 </script>
 
 <template>
-    <div class="flex flex-col gap-6 items-center justify-center h-64">
+    <div class="flex flex-col gap-6 items-center justify-center">
         <h1 class="text-2xl">Welcome In!</h1>
+        <button class="btn-primary" @click="addItem">Add something to the DB</button>
         <pre v-text="stories" />
     </div>
 </template>
