94b3c67b496c7c9037331b1a6d3690dffc11c2e7
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2023-05-09 00:52:36 -0600

    add a story details page

---
+++ pages/stories/[storyId].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { doc } from "firebase/firestore";
+
+const route = useRoute();
+
+const storyDoc = computed(() =>
+    doc(useFirestore(), "stories", route.params.storyId as string)
+);
+//TODO: if storyDoc doesn't exist, redirect to 404
+
+const story = useDocument(storyDoc);
+</script>
+
+<template>
+    <div>
+        <h1>story {{ story?.id }}</h1>
+        <h2 v-text="story?.name" />
+    </div>
+</template>
